eee2766d60557e9f6126837862ce0d80db2c966a
MSCINSYSTEMS-PC\MSC IN SYSTEMS <Ruben>
2020-10-16 14:34:41 -0430

    Creacion de Componente Parte 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Parte4Component } from './components/parte4/parte4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Parte4Component
   ],
   imports: [
     BrowserModule
---
+++ src/app/components/parte4/parte4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-parte4',
+  templateUrl: './parte4.component.html',
+  styleUrls: ['./parte4.component.scss']
+})
+export class Parte4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
